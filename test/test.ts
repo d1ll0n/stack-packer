526f2bbcca3733780742528412efd5acad442e25
dillon <dillonkellar@protonmail.com>
2020-04-23 22:08:31 -0500

    updated test

--- test/test.ts
+++ test/test.ts
@@ -6,6 +6,17 @@ import { AbiStruct, AbiEnum } from '../src/lib/types';
 
 const example = fs.readFileSync(path.join(__dirname, 'Example.sol'), 'utf8');
 const structs = parseCode(example);
-fs.writeFileSync('example-output.json', JSON.stringify(structs, null, 2))
+fs.writeFileSync('example-output-0.json', JSON.stringify(structs, null, 2))
 const gen = UnpackerGen.createLibrary(`Example`, <Array<AbiStruct | AbiEnum>> structs);
-fs.writeFileSync('example-gen.sol', gen);
+fs.writeFileSync('example-gen-0.sol', gen);
+
+const example1 = `
+struct ExampleWrapper {
+  uint32[2] x;
+  bytes32[3] y;
+}
+`;
+const structs1 = parseCode(example1);
+fs.writeFileSync('example-output-1.json', JSON.stringify(structs1, null, 2))
+const gen1 = UnpackerGen.createLibrary(`Example1`, <Array<AbiStruct | AbiEnum>> structs1);
+fs.writeFileSync('example-gen-1.sol', gen1);
