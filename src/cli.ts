517da14f531a09cbff88c1cdfeb1fbc197924217
dillon <dillonkellar@protonmail.com>
2020-04-26 19:44:14 -0500

    updated command line interface

--- src/cli.ts
+++ src/cli.ts
@@ -3,43 +3,84 @@ import fs from 'fs';
 import path from 'path';
 import { parseCode } from './parser/index';
 import { UnpackerGen } from './code-gen/sol-gen';
+import { buildLibrary } from './code-gen/ts-gen';
 import { AbiStruct, AbiEnum } from './lib/types';
 
 const argv = require('yargs')
   .usage('Usage: <lang> [options]')
-  .boolean('sol')
-  .default('sol', true)
-  .describe('sol', 'generate solidity library')
-  .option('input', {
-    alias: 'i',
-    describe: 'input file to read structs from',
-    demandOption: true
+  .command({
+    command: 'sol',
+    describe: 'generate solidity library',
+    builder: {
+      input: {
+        alias: ['i'],
+        describe: 'input file or directory to read solidity structs from',
+        demandOption: true,
+        coerce: path.resolve
+      },
+      output: {
+        alias: ['o', 'outDir'],
+        describe: 'file or directory to write generated code to',
+        demandOption: true,
+        coerce: path.resolve
+      },
+      verbose: {
+        alias: 'v',
+        describe: 'code verbosity',
+        default: false,
+        type: 'boolean'
+      }
+    }
   })
-  .option('output', {
-    alias: 'o',
-    describe: 'file to write generated code to',
-    demandOption: true
+  .command({
+    command: 'ts',
+    describe: 'generate typescript library',
+    builder: {
+      input: {
+        alias: ['i'],
+        describe: 'input file to read structs from',
+        demandOption: true,
+        coerce: path.resolve
+      },
+      output: {
+        alias: 'o',
+        describe: 'directory to write generated code to',
+        demandOption: true,
+        coerce: path.resolve
+      },
+      verbose: {
+        alias: 'v',
+        describe: 'code verbosity',
+        default: false,
+        type: 'boolean'
+      }
+    }
   })
-  .option('verbose', {
-    alias: 'v',
-    describe: 'code verbosity',
-    default: false,
-    type: 'boolean'
-  })
-  .coerce(['input', 'output'], path.resolve)
   .help('h')
   .fail(function(msg, err) {
-    if (err instanceof InputError) console.error('Input file not found.');
+    console.error(msg);
     process.exit(1);
   })
   .argv;
 
-class InputError extends Error {}
+if (!fs.existsSync(argv.input)) throw new Error(`File not found: ${argv.input}`);
+const code = fs.readFileSync(argv.input, 'utf8');
+const structs = <Array<AbiStruct | AbiEnum>> parseCode(code);
 
-if (argv.sol) {
-  if (!fs.existsSync(argv.input)) throw new InputError();
-  const code = fs.readFileSync(argv.input, 'utf8');
-  const structs = <Array<AbiStruct | AbiEnum>> parseCode(code);
+if (argv._.includes('sol')) {
   const lib = UnpackerGen.createLibrary('OutputCode', structs, { verbose: argv.verbose });
   fs.writeFileSync(argv.output, lib);
+}
+
+if (argv._.includes('ts')) {
+  const dir = argv.output;
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+  const files = buildLibrary(<Array<AbiStruct | AbiEnum>> structs);
+  const index = [];
+  files.forEach(({ code, fileName, jsonFile, jsonFileName }) => {
+    fs.writeFileSync(path.join(dir, fileName), code);
+    fs.writeFileSync(path.join(dir, jsonFileName), jsonFile);
+    index.push(`export * from './${fileName}';`);
+  });
+  fs.writeFileSync(path.join(dir, 'index.ts'), index.join('\n').replace(/\.ts/g, ''));
 }
