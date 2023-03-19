c082eb3dfcb3f74777ca3da0be86bff06206c0d7
d1ll0n <dillon@indexed.finance>
2023-03-19 10:54:40 -0500

    update project helpers

--- src/project/inputs.ts
+++ src/project/inputs.ts
@@ -1,34 +1,109 @@
-import fs from 'fs';
-import path from 'path';
-import { parseCode } from '../parser';
-import { AbiEnum, AbiStruct } from '../types';
-import { isDir, isSolFile } from './paths';
+import fs from "fs";
+import path from "path";
 
-export type FileWithStructs = { fileName: string; structs: Array<AbiStruct | AbiEnum> }
+import {
+  AbiEnum,
+  AbiError,
+  AbiEvent,
+  AbiFunction,
+  AbiStruct,
+  StateVariable,
+} from "../types";
+import { fromInterface } from "../parser/from-interface";
+import { parseCode } from "../parser";
+import { withSpaceOrNull } from "../lib/text";
 
-export const getInputFiles = (argv: { input: string }): FileWithStructs[] => {
+import { getAllSolidityFilesInDirectory, getExtension, isDir } from "./paths";
+
+export type FileWithStructs = {
+  filePath: string;
+  ext: string;
+  content?: string;
+  fileName: string;
+  contractName?: string;
+  structs: Array<AbiStruct | AbiEnum>;
+  functions: AbiFunction[];
+  stateVariables: StateVariable[];
+  errors: AbiError[];
+  events: AbiEvent[];
+};
+
+type Options = {
+  allowJson?: boolean;
+  allowSol?: boolean;
+  allowDirectory?: boolean;
+};
+
+export const getInputFiles = (
+  argv: { input: string },
+  { allowJson = true, allowSol = true, allowDirectory = true }: Options = {}
+): FileWithStructs[] => {
   if (!fs.existsSync(argv.input)) {
-    throw new Error(`File or directory not found: ${argv.input}`);
+    throw new Error(
+      `File${withSpaceOrNull(allowDirectory && " or directory")} not found: ${
+        argv.input
+      }`
+    );
   }
   const files: FileWithStructs[] = [];
   const filePaths: string[] = [];
   if (isDir(argv.input)) {
-    const fileNames = fs.readdirSync(argv.input).filter(isSolFile);
-    for (const fileName of fileNames) {
-      const filePath = path.join(argv.input, fileName);
-      filePaths.push(filePath);
+    if (!allowDirectory) {
+      throw Error(
+        `Input must be a file. Provided path was a directory: ${argv.input}`
+      );
     }
+    filePaths.push(...getAllSolidityFilesInDirectory(argv.input));
   } else {
-    if (!isSolFile(argv.input)) {
-      throw new Error(`${argv.input} is not a Solidity file`)
+    const allowedExtensions = [allowJson && ".json", allowSol && ".sol"].filter(
+      Boolean
+    );
+    const ext = getExtension(argv.input);
+    if (!allowedExtensions.includes(ext)) {
+      throw Error(
+        `Unsupported file type: ${
+          argv.input
+        }\nSupported file types: ${allowedExtensions.join(",")}`
+      );
     }
     filePaths.push(argv.input);
   }
   for (const filePath of filePaths) {
-    const { name } = path.parse(filePath);
-    const code = fs.readFileSync(filePath, 'utf8');
-    const structs = parseCode(code) as Array<AbiStruct | AbiEnum>;
-    files.push({ fileName: name, structs })
+    try {
+      const { name: fileName, ext } = path.parse(filePath);
+      if (ext === ".json") {
+        const jsonFile = require(filePath);
+        const fragments = Array.isArray(jsonFile) ? jsonFile : jsonFile.abi;
+        if (!fragments) {
+          throw Error(`ABI not found in ${filePath}`);
+        }
+        files.push({
+          filePath,
+          ext,
+          fileName,
+          stateVariables: [],
+          ...fromInterface(fragments),
+        });
+      } else {
+        const code = fs.readFileSync(filePath, "utf8");
+        const { functions, structs, stateVariables, errors, events, name } =
+          parseCode(code);
+        files.push({
+          filePath,
+          ext,
+          content: code,
+          fileName,
+          contractName: name,
+          structs: structs as Array<AbiStruct | AbiEnum>,
+          functions,
+          errors,
+          events,
+          stateVariables,
+        });
+      }
+    } catch (err) {
+      throw Error(`Error parsing file: ${filePath}\n` + err.message);
+    }
   }
   return files;
-}
+};
--- src/project/paths.ts
+++ src/project/paths.ts
@@ -1,49 +1,80 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
 export const getExtension = (_path: string) => path.parse(_path).ext;
 
-export const isSolFile = (_path: string) => getExtension(_path) === '.sol';
+export const isFile = (_path) => fs.statSync(_path).isFile();
 
-export const isDir = (_path: string) => !Boolean(getExtension(_path));
+export const isSolFile = (_path: string) =>
+  isFile(_path) && getExtension(_path) === ".sol";
 
-export const getDir = (_path: string) => isDir(_path) ? _path : path.parse(_path).dir;
+export const isDir = (_path: string) => fs.statSync(_path).isDirectory();
+
+export const getDir = (_path: string) =>
+  !path.parse(_path).ext ? _path : path.parse(_path).dir;
 
 export const parentOf = (_path: string) => path.parse(_path).dir;
 
-export const containsChild = (_path: string, child: string) => fs.existsSync(path.join(_path, child))
+export const containsChild = (_path: string, child: string) =>
+  fs.existsSync(path.join(_path, child));
 
 export const findDirectoryInTree = (_path: string, parentName: string) => {
   const parents = getDir(_path).split(path.sep);
   const indexOfParent = parents.lastIndexOf(parentName);
   if (indexOfParent < 0) return undefined;
-  return path.join(path.parse(_path).root, ...parents.slice(0, indexOfParent + 1));
-}
+  return path.join(
+    path.parse(_path).root,
+    ...parents.slice(0, indexOfParent + 1)
+  );
+};
 
-export const findChild = (_path: string, childOrPossibleChildren: string | string[]): string | undefined => {
+export const findChild = (
+  _path: string,
+  childOrPossibleChildren: string | string[]
+): string | undefined => {
   if (Array.isArray(childOrPossibleChildren)) {
-    return childOrPossibleChildren.find(child => findChild(_path, child))
+    return childOrPossibleChildren.find((child) => findChild(_path, child));
   }
   if (fs.existsSync(path.join(_path, childOrPossibleChildren))) {
-    return childOrPossibleChildren
+    return childOrPossibleChildren;
   }
-}
+};
 
 const recursiveMkDirIfNotExists = (target: string, original: string) => {
   if (!fs.existsSync(target)) {
     const parent = path.parse(target).dir;
     if (!parent) {
-      throw Error(`Failed to resolve existing parent for ${original}`)
+      throw Error(`Failed to resolve existing parent for ${original}`);
     }
     recursiveMkDirIfNotExists(parent, original);
     fs.mkdirSync(target);
   }
-}
+};
 
 export const mkdirIfNotExists = (target: string) => {
-  const targetDir = getDir(toAbsolutePath(target))
-  recursiveMkDirIfNotExists(targetDir, targetDir)
-  return targetDir
-}
+  const targetDir = getDir(toAbsolutePath(target));
+  recursiveMkDirIfNotExists(targetDir, targetDir);
+  return targetDir;
+};
 
-export const toAbsolutePath = (target: string) => path.isAbsolute(target) ? target : path.join(process.cwd(), target);
+export const toAbsolutePath = (target: string) =>
+  path.isAbsolute(target) ? target : path.join(process.cwd(), target);
+
+export const getAllSolidityFilesInDirectory = (target: string): string[] => {
+  const children = fs.readdirSync(target).map((p) => path.join(target, p));
+  if (!children) return [];
+  const grandChildren = children
+    .filter(isDir)
+    .reduce(
+      (arr, child) => [...arr, ...getAllSolidityFilesInDirectory(child)],
+      []
+    );
+  const solChildren = children.filter(isSolFile);
+  return [...solChildren, ...grandChildren];
+};
+
+export const getRelativePath = (from: string, to: string): string => {
+  let relative = path.relative(from, to);
+  if (!relative.startsWith("../")) relative = `./${relative}`;
+  return relative;
+};
